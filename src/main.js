3b6a3965c80be64f0fffbcab72a248ec968c8b53
moyo <moyosxtouch@gmail.com>
2023-12-06 14:03:35 -0600

    se agrego firebase al proyecto

--- src/main.js
+++ src/main.js
@@ -5,11 +5,17 @@ import { createPinia } from "pinia";
 import config from "../formkit.config";
 import App from "./App.vue";
 import router from "./router";
+import { VueFire, VueFireAuth } from "vuefire";
+import { firebaseApp } from "./config/firebase";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(plugin, defaultConfig(config));
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+});
 
 app.mount("#app");
