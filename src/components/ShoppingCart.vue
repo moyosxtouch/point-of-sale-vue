14d0f1918abe9273e8b85846c79d058ba4d52f21
moyo <moyosxtouch@gmail.com>
2023-12-12 17:31:51 -0600

    se realizo modificacion para mostrar el total a pagar

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useCartStore } from "../stores/cart";
 import ShoppingCartItem from "./ShoppingCartitem.vue";
+import Amount from "./Amount.vue";
+import { formatCurrency } from "../helpers";
 const cart = useCartStore();
 </script>
 
@@ -17,5 +19,21 @@ const cart = useCartStore();
         :item="item"
       />
     </ul>
+    <dl
+      class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500"
+    >
+      <Amount>
+        <template #label>Subtotal:</template>
+        {{ formatCurrency(cart.subtotal) }}
+      </Amount>
+      <Amount>
+        <template #label>Impuestos:</template>
+        {{ formatCurrency(cart.taxes) }}
+      </Amount>
+      <Amount>
+        <template #label>Total:</template>
+        {{ formatCurrency(cart.total) }}
+      </Amount>
+    </dl>
   </div>
 </template>
